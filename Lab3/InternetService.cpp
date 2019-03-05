d1bc8a1bd86dac794d434a0fb936da6cd0f43b73
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-03-05 08:29:09 -0600

    Comments on Lab 3(Previous Code)

--- Lab3/InternetService.cpp
+++ Lab3/InternetService.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
-
+//Main method returning  0 for code run
 int main(){
+    // Asking user for package plan and hours they spent per month. Initilizing the input in variables
     cout<<"Enter the letter of the package (A, B or C): ";
     char choice;
     cin>>choice;
@@ -9,7 +10,9 @@ int main(){
     double hours;
     cin>>hours;
     double cost;
+    //Switch statement to check the plan choice A, B and C. Run the appropriate switch case
     switch (choice){
+        //Internal switch case that has boolean for test case and performs calculation based on the boolean value hours>plan hours
         case 'A': switch (hours>10.0){
                     case true: cost=9.95+(hours-10.0)*2.0;
                         break;
@@ -27,6 +30,7 @@ int main(){
         case 'C': cost =19.95;
             break;
     }
+    //Printing the result at end for output purpose.
     printf("The total charge is $%.2f.",cost);
     return 0;
 }
